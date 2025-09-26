36bb974cf7dab519b147209b9582b53054f9d9d2
huy diem dam <huyhoquang01@gmail.com>
2025-09-26 20:06:17 +0700

    học heap #1

---
+++ heapq/main.py
@@ -0,0 +1,23 @@
+import heapq
+
+num = [5, 3, 8, 1, 2, 7, 4, 6, 9]
+heapq.heapify(num)
+# print(num) -> [1, 2, 4, 3, 5, 7, 8, 6, 9], vì sao?
+# đây là một list bình thường, không phải đã sắp xếp
+# đồng thời là một heap, cấu trúc dữ liệu đặc biệt
+# để là 1 heap thỏa mãn 2 yêu cầu: node cha luôn nhỏ hơn node con
+# và cây luôn được lấp đầy từ trái sang phải
+# heapq là một min-heap, phần tử nhỏ nhất luôn ở đầu
+# heaify tạo ra một heap từ một list bất kỳ
+# cho nên heapq.heapify tạo ra mảng như trên
+#               1
+#            /     \
+#           2       4
+#        /   \     / \
+#       3     5   7   8
+#     /  \  
+#    6    9      
+#
+for i in range(len(num)):
+    print(heapq.heappop(num))
+# now the list output is ordered
