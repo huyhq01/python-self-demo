43c69b02e0b4b0d4b5c63d4ac771fcff45c45da2
huy diem dam <huyhoquang01@gmail.com>
2025-09-27 19:25:45 +0700

    giải encode decode string

---
+++ leetcode/encode_decode_string.py
@@ -0,0 +1,57 @@
+# Design an algorithm to encode a list of strings to a single string.
+# The encoded string is then decoded back to the original list of strings.
+# Please implement encode and decode
+
+# Example 1:
+# Input: ["neet","code","love","you"]
+# Output:["neet","code","love","you"]
+
+# Example 2:
+# Input: ["we","say",":","yes"]
+# Output: ["we","say",":","yes"]
+
+# Constraints:
+# 0 <= strs.length < 100
+# 0 <= strs[i].length < 200
+# strs[i] contains only UTF-8 characters.
+
+
+def encode(strs: list[str]) -> str:
+    encoded_str = ""
+    for word in strs:
+        encoded_str += f"{len(word)}#{word}"
+    return encoded_str
+
+
+# mỗi từ sẽ được encode thành: "chiều dài của từ + '#' + từ"
+# ví dụ "we" -> "2#we", "say" -> "3#say"
+
+
+def decode(s: str) -> list[str]:
+    i = 0
+    decoded_list = []
+    while i < len(s):
+        # chiều dài của từ
+        word_length = s[i : s.find("#", i)] 
+        # tìm ví trí của kí tự '#' vì chiều dài của từ có thể là vài chục, trăm
+        
+        # ví trí chữ cái đầu tiên của từ
+        start = i + 1 + len(word_length)
+
+        # vị trí của kí tự cuối cùng của từ + 1 => tìm vị trí độ dài từ tiếp theo
+        end = start + int(word_length)
+
+        # lấy từ
+        word = s[start:end]
+        
+        decoded_list.append(word)
+        i = end
+    return decoded_list
+
+
+def solution(list_str):
+    print("encode: ", encode(list_str))
+    return decode(encode(list_str))
+
+
+print("decode: ", solution(["we", "say", ":", "yes", "!@#$%^&*()"]))
